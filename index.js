eabe08245435d16fed879d826ef3f4069c566d01
Tatev <tatevs85@gmail.com>
2021-03-19 23:59:53 +0400

    Массивы

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ let shoppingCart = fruits;
 shoppingCart.push("Банан");
 
 
-alert( fruits.length ); // ? =>4
+console.log( fruits.length ); // ? =>4
 
 let styles=["Джаз", "Блюз"];
 styles.push('Рок-н-ролл')
 styles[Math.floor((styles.length - 1) / 2)] = "Классика";
 styles.shift()
 styles.unshift('Рэп','Регги')
+
+
+let arr=[1,3,5,7]
+
+console.log(arr.concat(9,11))
