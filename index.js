45dc7b7468e091b2aa2039017829666089e40f71
Tatev <tatevs85@gmail.com>
2021-03-20 00:08:40 +0400

    forEach

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-let fruits = ["Яблоки", "Груша", "Апельсин"];
-
-let shoppingCart = fruits;
-shoppingCart.push("Банан");
-
-
-console.log( fruits.length ); // ? =>4
-
-let styles=["Джаз", "Блюз"];
-styles.push('Рок-н-ролл')
-styles[Math.floor((styles.length - 1) / 2)] = "Классика";
-styles.shift()
-styles.unshift('Рэп','Регги')
-
-
-let arr=[1,3,5,7]
-
-console.log(arr.concat(9,11))
+["hello", "hi", "բարև"].forEach((item, index, array) => {
+    console.log(`${item} , ${index} : ${array}`);
+  });
