c32aeb37dfe128c221135f4d30934620c898d20b
Tatev <tatevs85@gmail.com>
2021-03-20 00:38:13 +0400

    date

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-let str = "ASDFGHJ";
+//   let map=Object.fromEntries(basket.entries())
+//   console.log(map.orange)
 
-console.log( str.split('') );
+function getLocalDay(date) {
 
-
-let prices = Object.fromEntries([
-    ['banana', 1],
-    ['orange', 3],
-    ['apple', 5]
-  ]);
+    let day = date.getDay();
   
+    if (day == 0) { 
+      day = 7;
+    }
   
-  console.log(prices.apple);
-
-  let basket=new Map()
-  basket.set('banana', 1);
-  basket.set('orange', 3)
-  basket.set('apple', 5)
-
-  let map=Object.fromEntries(basket.entries())
-  console.log(map.orange)
+    return day;
+  }
+  let date = new Date(2021, 2, 21);  //march  21 sunday 
+  console.log( getLocalDay(date) )
