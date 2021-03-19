91a8e599b1728d3ade3f26b6e0fa146d57928077
Tatev <tatevs85@gmail.com>
2021-03-20 00:25:07 +0400

    Map

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-["hello", "hi", "բարև"].forEach((item, index, array) => {
-    console.log(`${item} , ${index} : ${array}`);
-  });
+let str = "ASDFGHJ";
+
+console.log( str.split('') );
+
+
+let prices = Object.fromEntries([
+    ['banana', 1],
+    ['orange', 3],
+    ['apple', 5]
+  ]);
+  
+  
+  console.log(prices.apple);
+
+  let basket=new Map()
+  basket.set('banana', 1);
+  basket.set('orange', 3)
+  basket.set('apple', 5)
+
+  let map=Object.fromEntries(basket.entries())
+  console.log(map.orange)
