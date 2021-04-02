c237e0acc0ba28c478122dcf6a1ba0fc413665b8
Tatev <tatevs85@gmail.com>
2021-04-03 01:50:14 +0400

    Замыкание

--- Замыкание.js
+++ Замыкание.js
@@ -1,25 +1,35 @@
-let name = "John";
+// let name = "John";
 
-function sayHi() {
-  console.log("Hi, " + name);
-}
+// function sayHi() {
+//   console.log("Hi, " + name);
+// }
 
-name = "Pete";
+// name = "Pete";
 
-sayHi()
+// sayHi()
 
-function makeWorker() {
-    let myname = "Pete";
+// function makeWorker() {
+//     let myname = "Pete";
   
-    return function() {
-      console.log(myname);
-    };
+//     return function() {
+//       console.log(myname);
+//     };
+//   }
+  
+//   let myname = "John";
+  
+//   // create a function
+//   let work = makeWorker();
+  
+//   // call it
+//   work();
+
+
+  function inBetween(a,b) {
+      return function (x) {
+          return x>=a && x<=b
+      }
+      
   }
-  
-  let myname = "John";
-  
-  // create a function
-  let work = makeWorker();
-  
-  // call it
-  work();
+  let array= [1, 2, 3, 4, 5, 6, 7];
+console.log( array.filter(inBetween(1, 4)) )
