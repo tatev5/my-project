2bc7ae0ce9b7acf9b5882845304712ff263af8aa
Tatev <tatevs85@gmail.com>
2021-04-13 23:29:22 +0400

    sum

--- quiz.js
+++ quiz.js
@@ -23,3 +23,12 @@ let getPath = take(obj, ['a', '0', 'b', 'c']);
 
 
 console.log(getPath);
+
+function add(param1, param2) {
+    
+    if((-1000 <= param1 || param1 >= 1000) && (-1000 <= param2 || param2 >= 1000)) {
+       var sum=param1+param2;
+    }return sum
+}
+var sum=add(2,4)
+console.log(sum)
