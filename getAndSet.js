76e77a1ac032f009a5f4f3bc3dbe299658a5529a
Tatev <tatevs85@gmail.com>
2021-04-14 00:27:31 +0400

    this

--- getAndSet.js
+++ getAndSet.js
@@ -10,3 +10,12 @@ const person={
  }
  person.lastnameChange='',
 console.log(person.lastnameChange)
+
+
+function User(name,surname,age){
+    this.name=name;
+    this.surname=surname;
+    this.age=age
+}
+let Lily=new User("Lily","Smith",20)
+console.log('Name:' +Lily.name + ' Surname:' + Lily.surname + ' age:'+Lily.age )
