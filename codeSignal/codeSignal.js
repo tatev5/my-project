f1240f90fec45a1184605bbddedb9b4d4d100c47
Tatev <tatevs85@gmail.com>
2021-05-15 18:23:44 +0400

    task2

--- codeSignal/codeSignal.js
+++ codeSignal/codeSignal.js
@@ -8,3 +8,10 @@ function add(param1, param2) {
 var sum=add(1,2)
 console.log(sum)
 
+//2
+function centuryFromYear(year) {
+    return Math.floor(year/100) + ((year%100) ? 1 : 0)
+}
+
+
+console.log(centuryFromYear(2000))
