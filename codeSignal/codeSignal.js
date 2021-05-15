c59bc40d89298bb2003ef47dd9681967bcb9478b
Tatev <tatevs85@gmail.com>
2021-05-15 18:22:39 +0400

    task1

---
+++ codeSignal/codeSignal.js
@@ -0,0 +1,10 @@
+//1
+function add(param1, param2) {
+    
+    if((-1000 <= param1 || param1 >= 1000) && (-1000 <= param2 || param2 >= 1000)) {
+       var sum=param1+param2;
+    }return sum
+}
+var sum=add(1,2)
+console.log(sum)
+
